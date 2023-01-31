5115f9b9ca77487cc3ea707e39dc19b72d13700c
zhangqiang <z397435052@gmail.com>
2023-01-31 11:00:30 +0800

    test: 单元测试

--- response_test.go
+++ response_test.go
@@ -18,3 +18,18 @@ func TestNew400Response(t *testing.T) {
 	err = Wrap400Err(err, 4)
 	t.Log(err.Error())
 }
+
+func TestNewError(t *testing.T) {
+	e := NewError(1, fmt.Errorf("err1"))
+	t.Log(e.Error())
+}
+
+func TestNewMsg(t *testing.T) {
+	e := NewMsg(1, "a,%d", 2)
+	t.Log(e.Error())
+}
+
+func TestNewErrorMsg(t *testing.T) {
+	e := NewErrorMsg(1, fmt.Errorf("err1"), "a,%d", 2)
+	t.Log(e.Error())
+}
